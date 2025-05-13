4697dd1d8aa267a0e0b79f776c72c0e4973a90e1
jamie-kaemmnitz <jamiekaemmnitz24@gmail.com>
2025-05-13 20:32:03 +0100

    grabbed elements for java

---
+++ java/main.js
@@ -0,0 +1,44 @@
+const startButton = document.getElementById('start-button');
+const nextQuestionButton = document.getElementsByClassName('next-question');
+const continueButton = document.getElementsByClassName('continue-button');
+const checkScoreButton = document.getElementById('check-score');
+
+const arsenalQuestion = document.getElementById('arsenal');
+const astonVillaQuestion = document.getElementById('aston-villa');
+const bournemouthQuestion = document.getElementById('bournemouth');
+const brentfordQuestion = document.getElementById('brentford');
+const brightonQuestion = document.getElementById('brighton');
+const chelseaQuestion = document.getElementById('chelsea');
+const crystalPalaceQuestion = document.getElementById('crystal-palace');
+const evertonQuestion = document.getElementById('everton');
+const fulhamQuestion = document.getElementById('fulham');
+const ipswichQuestion = document.getElementById('ipswich');
+const leicesterQuestion = document.getElementById('leicester');
+const liverpoolQuestion = document.getElementById('liverpool');
+const manCityQuestion = document.getElementById('man-city');
+const manUtdQuestion = document.getElementById('man-utd');
+const newcastleQuestion = document.getElementById('newcastle');
+const nottinghamForestQuestion = document.getElementById('nottingham-forest');
+const southamptonQuestion = document.getElementById('southampton');
+const tottenhamQuestion = document.getElementById('tottenham');
+const westHamQuestion = document.getElementById('west-ham');
+const wolvesQuestion = document.getElementById('wolves');
+
+const correctAnswers = document.getElementsByClassName('correct-answers');
+const wrongAnswer = document.getElementsByClassName('wrong-answers');
+
+const youScored = document.getElementById('you-scored');
+const computerScored = document.getElementById('computer-scored');
+const endOfQuiz = document.getElementById('end-of-quiz');
+
+const fiveNilScore = document.getElementById('five-nil');
+const fourNilScore = document.getElementById('four-nil');
+const fourOneScore = document.getElementById('four-one');
+const threeOneScore = document.getElementById('three-one');
+const threeTwoScore = document.getElementById('three-two');
+const twoTwoScore = document.getElementById('two-two');
+const twoThreeScore = document.getElementById('two-three');
+const oneThreeScore = document.getElementById('one-three');
+const oneFourScore = document.getElementById('one-four');
+const nilFourScore = document.getElementById('nil-four');
+const nilFiveScore = document.getElementById('nil-five');
